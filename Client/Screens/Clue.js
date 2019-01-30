97d4fdccae7623c2ca98dd769c1570ae3a3c07dd
Joy Grunklee <jgrunklee@gmail.com>
2019-01-30 14:45:41 -0800

    consolidate styles

--- Client/Screens/Clue.js
+++ Client/Screens/Clue.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Button, Image, ScrollView, Text, View } from 'react-native'
+import { styles } from '../../assets/styles';
 
 const Clue = (props) => {
   return (
@@ -14,7 +15,7 @@ const Clue = (props) => {
             source={require('../../assets/1234.jpg')}
             style={styles.winImage}
           />
-          <View style={styles.startButtonContainer}>
+          <View style={styles.greenButtonContainer}>
             <Button
               title="Continue"
               onPress={() => { props.navigation.navigate('Map') }}
@@ -26,65 +27,5 @@ const Clue = (props) => {
     </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  clueContainer: {
-    display: 'flex',
-    backgroundColor: 'green',
-    flexDirection: 'column',
-    height: '100%',
-    width: '100%',
-    padding: 50,
-  },
-  headerContainer: {
-    backgroundColor: 'yellow',
-    width: '100%',
-    borderRadius: 5,
-    // borderWidth: 3,
-    // borderColor: 'black',
-    padding: 20,
-  },
-  header: {
-    fontSize: 30,
-    color: 'black',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-  winImage: {
-    marginBottom: 30,
-    width: 270
-  },
-  bodyContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 30,
-  },
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-  start: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'green'
-  },
-  startButtonContainer: {
-    backgroundColor: 'goldenrod',
-    padding: 10,
-    borderRadius: 5,
-    // borderWidth: 3,
-    // borderColor: 'black',
-  },
-  startButton: {
-    color: 'black'
-  },
-});
 
 export default Clue
---
+++ assets/styles.js
@@ -0,0 +1,93 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    padding: 20,
+  },
+  largeText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  medText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    flex: 2,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  clueContainer: {
+    display: 'flex',
+    backgroundColor: 'orange',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    padding: 50,
+  },
+  headerContainer: {
+    backgroundColor: 'yellow',
+    width: '100%',
+    borderRadius: 5,
+    padding: 20,
+  },
+  header: {
+    fontSize: 30,
+    color: 'black',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  winImage: {
+    marginBottom: 30,
+    width: 270
+  },
+  winContainer: {
+    display: 'flex',
+    backgroundColor: 'goldenrod',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    padding: 50,
+  },
+  bodyContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30,
+  },
+  start: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green'
+  },
+  greenButtonContainer: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  redButtonContainer: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  startButton: {
+    color: 'black'
+  },
+});
+
