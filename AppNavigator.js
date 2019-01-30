c0fb05a725d23c62d673b050ce42e8ade0dac7cb
Joy Grunklee <jgrunklee@gmail.com>
2019-01-29 21:45:22 -0800

    Add clue component

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,11 +1,13 @@
-import {Home, StoryConcept, Map} from './Screens';
+import {Home, StoryConcept, Map, Clue, Win} from './Screens';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 //when I load the app with this, whatever is first shows up
 const RootStack = createStackNavigator({
   Home: Home,
   StoryConcept: StoryConcept,
-  Map: Map
+  Map: Map,
+  Clue: Clue,
+  Win: Win
 });
 
 const AppNavigator = createAppContainer(RootStack);
--- Screens/index.js
+++ Screens/index.js
@@ -2,4 +2,5 @@ import Home from './Home';
 import StoryConcept from './StoryConcept';
 import Win from './Win';
 import Map from './Map';
-export { Map, Win, StoryConcept, Home };
+import Clue from './Clue';
+export { Clue, Map, Win, StoryConcept, Home };
