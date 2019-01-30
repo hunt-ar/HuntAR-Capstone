f83347ac0922d8320a80cce87827bff36a2875f8
Frank Compston <compstonfrank@gmail.com>
2019-01-30 09:43:16 -0800

    feature: map, style: map

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,12 +1,15 @@
-import {Home, StoryConcept, Map} from './Screens';
+import { Home, StoryConcept, Map } from './Screens';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 //when I load the app with this, whatever is first shows up
-const RootStack = createStackNavigator({
-  Home: Home,
-  StoryConcept: StoryConcept,
-  Map: Map
-});
+const RootStack = createStackNavigator(
+  {
+    Home: Home,
+    StoryConcept: StoryConcept,
+    Map: Map
+  },
+  { headerMode: 'none' }
+);
 
 const AppNavigator = createAppContainer(RootStack);
 export default AppNavigator;
