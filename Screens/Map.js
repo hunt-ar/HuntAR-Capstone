8552f49c0ce960c957b0625c67b88b9041840d9a
Frank Compston <compstonfrank@gmail.com>
2019-01-29 17:15:55 -0800

    Half way there

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class App extends React.Component {
   constructor() {
@@ -12,26 +12,38 @@ export default class App extends React.Component {
         latitudeDelta: 0,
         longitudeDelta: 0,
         error: null
-      }
+      },
+      markers: [
+        {
+          latlng: {
+            latitude: 35.334835915305,
+            longitude: -120.74445785104
+          },
+          title: 'Code One',
+          description: 'How many banana'
+        }
+      ]
     };
     this.onRegionChange = this.onRegionChange.bind(this);
     this.getInitialState = this.getInitialState.bind(this);
   }
 
   async componentDidMount() {
-      await navigator.geolocation.getCurrentPosition((position) => {
+    await navigator.geolocation.getCurrentPosition(
+      position => {
         this.setState({
           region: {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
             latitudeDelta: 0.000922,
             longitudeDelta: 0.000421,
-            error: null,
+            error: null
           }
         });
       },
-        (error) => this.setState({ error: error }),
-        { enableHighAccuracy: true, timeout: 1000, maximumAge: 0 })
+      error => this.setState({ error: error }),
+      { enableHighAccuracy: true, timeout: 1000, maximumAge: 0 }
+    );
   }
 
   getInitialState() {
@@ -50,29 +62,26 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log('latitude', this.state.region.latitude)
-    console.log('longitude', this.state.region.longitude)
     return (
       <View style={styles.container}>
         {this.state.region.latitude ? (
-        <MapView
-          style={styles.map}
-          region={this.state.region}
-          onRegionChange={this.onRegionChange}
-          showsUserLocation
-        >
-          {/* {this.state.markers.map(marker => (
-            <Marker
-              coordinate={marker.latlng}
-              // title={marker.title}
-              // description={marker.description}
-            />
-          ))} */}
-        </MapView>
+          <MapView
+            style={styles.map}
+            region={this.state.region}
+            onRegionChange={this.onRegionChange}
+            showsUserLocation
+          >
+            {this.state.markers.map(marker => (
+              <Marker
+                key={1}
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+              />
+            ))}
+          </MapView>
         ) : (
-          <Text>
-            Loading...
-          </Text>
+          <Text>Loading...</Text>
         )}
       </View>
     );
