c79c9c36711d2d009ef6a3214601a94e48dcbc1f
Kristi Kelly <kristikelly2001@yahoo.com>
2019-02-08 22:06:18 -0800

    updated login, signup pages styling

--- Client/Screens/Home.js
+++ Client/Screens/Home.js
@@ -29,7 +29,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.HomeContainer}>
-        <Text style={styles.HomeHeader}>DisARm</Text>
+        <Text style={styles.HomeHeader}>disARm</Text>
         <View style={styles.HomeImage}>{this.renderImage()}</View>
         <View style={styles.HomeButtons}>
           <AwesomeButton
--- Client/Screens/SignUp.js
+++ Client/Screens/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Image,
@@ -10,30 +10,29 @@ import {
   ActivityIndicator,
   Alert,
   ScrollView
-} from 'react-native';
+} from "react-native";
 import {
   RkButton,
   RkText,
   RkTextInput,
   RkStyleSheet,
   RkAvoidKeyboard,
-} from 'react-native-ui-kitten';
-import { GradientButton } from '../Components';
-import { scaleVertical } from '../utils/scale';
-import firebase from 'firebase'
-import NavigationType from '../../config/navigation/propTypes';
-import { db } from '../store'
+  RkTheme
+} from "react-native-ui-kitten";
+import { GradientButton } from "../Components";
+import { scaleVertical } from "../utils/scale";
+import firebase from "firebase";
+import NavigationType from "../../config/navigation/propTypes";
+import { db } from "../store";
 
 export class SignUp extends React.Component {
-
-  state = { username: '', email: '', password: '', loading: false }
-
+  state = { username: "", email: "", password: "", loading: false };
 
   static navigationOptions = {
-    header: null,
+    header: null
   };
   static propTypes = {
-    navigation: NavigationType.isRequired,
+    navigation: NavigationType.isRequired
   };
 
   handleSignUp = () => {
@@ -42,131 +41,181 @@ export class SignUp extends React.Component {
 
     this.setState({ loading: true });
 
-    firebase.auth().onAuthStateChanged(function (user) {
-      db.collection('users').doc(user.uid).set({
-        username,
-        email
-      })
-    })
+    firebase.auth().onAuthStateChanged(function(user) {
+      db.collection("users")
+        .doc(user.uid)
+        .set({
+          username,
+          email
+        });
+    });
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(() => { this.setState({ loading: false }); })
-      .then(() => { this.props.navigation.navigate('Welcome'); })
-      .catch((error) => {
-        //Login was not successful.
-        this.setState({ loading: false })
-        Alert.alert(`We are unable to process your request at this time. ${error}`);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ loading: false });
       })
-  }
+      .then(() => {
+        this.props.navigation.navigate("Welcome");
+      })
+      .catch(error => {
+        //Login was not successful.
+        this.setState({ loading: false });
+        Alert.alert(
+          `We are unable to process your request at this time. ${error}`
+        );
+      });
+  };
 
   renderImage = () => (
-    <Image
-      style={styles.image}
-      source={require('../../assets/bomb.png')
-      }
-    />
+    <Image style={styles.image} source={require("../../assets/bomb.png")} />
   );
 
   render() {
     return this.state.loading === false ? (
       <ScrollView>
-
-
         <RkAvoidKeyboard
           style={styles.screen}
           onStartShouldSetResponder={() => true}
-          onResponderRelease={() => Keyboard.dismiss()}>
-          <View style={{ alignItems: 'center' }}>
+          onResponderRelease={() => Keyboard.dismiss()}
+        >
+          <View style={{ alignItems: "center" }}>
             {this.renderImage()}
-            <RkText rkType='h1'>Registration</RkText>
+            <RkText style={styles.headerText}>Registration</RkText>
           </View>
           <View style={styles.content}>
             <View>
-              <RkTextInput rkType='rounded' placeholder='Username' onChangeText={username => this.setState({ username })} value={this.state.username} />
-
-              <RkTextInput rkType='rounded' placeholder='Email' onChangeText={email => this.setState({ email })} value={this.state.email} />
-
-              <RkTextInput rkType='rounded' placeholder='Password' secureTextEntry onChangeText={password => this.setState({ password })} value={this.state.password} />
-
-              <Button
-                style={styles.save}
-                title='SIGN UP'
-                onPress={this.handleSignUp}
+              <RkTextInput
+                rkType="frame"
+                placeholder="Username"
+                onChangeText={username => this.setState({ username })}
+                value={this.state.username}
               />
-            </View>
 
+              <RkTextInput
+                rkType="frame"
+                placeholder="Email"
+                onChangeText={email => this.setState({ email })}
+                value={this.state.email}
+              />
+
+              <RkTextInput
+                rkType="frame"
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={password => this.setState({ password })}
+                value={this.state.password}
+              />
+              <View style={styles.textRow}>
+                <RkButton
+                  style={{ backgroundColor: "#004466", width: 150 }}
+                  title="SIGN UP"
+                  onPress={this.handleSignUp}
+                >
+                  SIGN UP
+                </RkButton>
+              </View>
+            </View>
             <View style={styles.footer}>
               <View style={styles.textRow}>
-                <RkText rkType='primary3'>Already have an account?</RkText>
+                <RkText rkType="primary3"
+                 style={{ color: "#ff4d4d" }}>Already have an account?</RkText>
               </View>
-              <View>
-                <Button
+              <View style={styles.textRow}>
+                <RkButton
+                  style={{ backgroundColor: "#004466", width: 150 }}
                   onPress={() => {
-                    this.props.navigation.navigate('Login');
+                    this.props.navigation.navigate("Login");
                   }}
                   title="Sign In"
-                  style={styles.save}
-                />
+                >
+                  Sign In
+                </RkButton>
               </View>
             </View>
 
             <View style={styles.footer}>
-              <View>
-                <Button
+            <View style={styles.textRow}>
+                <RkButton
+                  style={{ backgroundColor: "#004466", width: 150 }}
                   onPress={() => {
-                    this.props.navigation.navigate('Home');
+                    this.props.navigation.navigate("Home");
                   }}
                   title="Go Back"
-                  style={styles.save}
-                />
+                >
+                  Go Back
+                </RkButton>
               </View>
             </View>
           </View>
         </RkAvoidKeyboard>
       </ScrollView>
     ) : (
-        <View style={styles.loadingContainer}>
-          <Text>Loading</Text>
-          <ActivityIndicator size="large" />
-        </View>
-      )
+      <View style={styles.loadingContainer}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    );
   }
 }
+RkTheme.setType("RkTextInput", "frame", {
+  input: {
+    backgroundColor: "white",
+    marginLeft: 0,
+    marginHorizontal: 0,
+    borderRadius: 5
+  },
+  color: "gray",
+  backgroundColor: "white",
+  borderRadius: 10,
+  container: {
+    paddingHorizontal: 20
+  }
+});
 
 const styles = RkStyleSheet.create(theme => ({
   screen: {
-    padding: 16,
+    padding: scaleVertical(16),
     flex: 1,
-    justifyContent: 'space-around',
-    backgroundColor: theme.colors.screen.base,
+    justifyContent: "space-between",
+    backgroundColor: "#004466"
   },
   image: {
     marginBottom: 10,
-    height: scaleVertical(77),
-    resizeMode: 'contain',
+    height: scaleVertical(190),
+    resizeMode: "contain"
   },
   content: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between"
   },
   save: {
-    marginVertical: 20,
+    marginVertical: 20
+  },
+  headerText: {
+    fontSize: 20,
+    color: "white",
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontWeight: "bold"
   },
   buttons: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 24,
     marginHorizontal: 24,
-    justifyContent: 'space-around',
+    justifyContent: "space-around"
   },
   footer: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
   textRow: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    color: "#ff4d4d"
   },
   loadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   }
 }));
